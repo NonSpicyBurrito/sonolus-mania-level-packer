8450e262ca9581b9d716f95ad05ff1f28b048e59
Burrito <burritoyangster@gmail.com>
2024-10-12 01:34:23 +0800

    Implemented Mania

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { version } from '@sonolus/core'
+import { version as engineVersion } from 'sonolus-mania-engine'
 import { ref } from 'vue'
 import { version as appVersion } from '../package.json'
 import VLink from './components/VLink.vue'
@@ -16,6 +17,8 @@ const rating = ref(0)
 const cover = ref<File>()
 const bgm = ref<File>()
 const preview = ref<File>()
+const chart = ref<File>()
+const offset = ref(0)
 const description = ref('')
 
 const state = ref<{ type: 'packing' } | { type: 'success' } | { type: 'error'; error: unknown }>()
@@ -32,6 +35,8 @@ const onPack = async () => {
             cover: cover.value,
             bgm: bgm.value,
             preview: preview.value,
+            chart: chart.value,
+            offset: offset.value,
             description: description.value,
         })
 
@@ -53,18 +58,22 @@ const onClose = () => {
 </script>
 
 <template>
-    <h1 class="text-center text-xl font-bold sm:text-3xl">Sonolus Level Packer</h1>
+    <h1 class="text-center text-xl font-bold sm:text-3xl">Sonolus Mania Level Packer</h1>
 
     <p>
-        See README.md. <br />
+        Pack your Mania levels into Sonolus collection packages. <br />
         <br />
         Sonolus: <br />
         Version {{ version.sonolus }} <br />
         <VLink url="https://sonolus.com" /> <br />
         <br />
+        Engine: <br />
+        Version {{ engineVersion }} <br />
+        <VLink url="https://github.com/NonSpicyBurrito/sonolus-mania-engine" /> <br />
+        <br />
         Packer: <br />
         Version {{ appVersion }} <br />
-        <VLink url="https://github.com/Sonolus/sonolus-level-packer" />
+        <VLink url="https://github.com/NonSpicyBurrito/sonolus-mania-level-packer" />
     </p>
 
     <form @submit.prevent="onPack">
@@ -95,8 +104,8 @@ const onClose = () => {
                 label="Rating"
                 placeholder="Enter level rating..."
                 :min="0"
-                :max="100"
-                :step="1"
+                :max="10"
+                :step="0.01"
             />
 
             <VFileField v-model="cover" label="Cover" accept="image/*" />
@@ -104,6 +113,10 @@ const onClose = () => {
             <VFileField v-model="bgm" label="Bgm" />
 
             <VFileField v-model="preview" label="Preview" />
+
+            <VFileField v-model="chart" label="Chart (OSU)" accept=".osu,.txt" />
+
+            <VNumberField v-model="offset" label="Offset" placeholder="Enter level offset..." />
 
             <VTextAreaField
                 v-model="description"
--- src/packer/level.ts
+++ src/packer/level.ts
@@ -1,5 +1,11 @@
+import { mcToLevelData, osuToMC } from 'sonolus-mania-engine'
 import type { PackLevelData } from '.'
 
-export const packLevelData: PackLevelData = () => {
-    throw new Error('packLevelData not implemented')
+export const packLevelData: PackLevelData = async ({ chart, offset }) => {
+    if (!chart) throw new Error('No chart file selected')
+
+    const mc = osuToMC(await chart.text())
+    const levelData = mcToLevelData(mc, offset)
+
+    return { type: 'json', data: levelData }
 }
